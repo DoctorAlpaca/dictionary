fe69ee36cf7ab35956634a80ada83e3d1bd73ecc
cofl <lanerdofchristian@gmail.com>
2014-11-23 14:14:56 -0500

    For some reason this was left out.

---
+++ src/de/eric_wiltfang/dictionary/local/IDle.java
@@ -0,0 +1,25 @@
+package de.eric_wiltfang.dictionary.local;
+
+import de.eric_wiltfang.dictionary.Dictionary;
+import de.eric_wiltfang.dictionary.DictionaryMainWindow;
+
+import javax.swing.JFrame;
+import java.awt.*;
+import java.io.File;
+
+/**
+ * @author cofl
+ *
+ */
+public class Idle {
+    public static String[] argsHolder;
+    public static File dict = null;
+    public static Dictionary dic = null;
+    public static Point location = null;
+
+    public static void restart(JFrame window){
+        location = window.getLocation();
+        window.dispose();
+        DictionaryMainWindow.main(argsHolder);
+    }
+}
